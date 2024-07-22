b5540c13cd642a9d6de095658705386beb5ddd3f
ysungwon <hwoarang09@naver.com>
2024-07-23 01:14:21 +0900

    propertyList도 axios상태 보장

--- src/pages/Property/components/PropertyList.js
+++ src/pages/Property/components/PropertyList.js
@@ -5,16 +5,24 @@ import { useState, useEffect } from "react";
 const URL = "http://localhost:3002/opn";
 function PropertyList() {
   const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(true); // 로딩 상태 추가
+  const [error, setError] = useState(null); // 에러 상태 추가
 
   const fetchProperties = async () => {
-    const response = await axios.get(URL);
-
-    setProperties(response.data);
+    try {
+      const response = await axios.get(URL);
+      setProperties(response.data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false); // 로딩 완료
+    }
   };
 
   useEffect(() => {
     fetchProperties();
   }, []);
+
   const editPropertyById = async (id, formData) => {
     const response = await axios.put(URL + "/" + id, formData);
 
@@ -28,7 +36,16 @@ function PropertyList() {
 
     setProperties(updatedProperties);
   };
-console.log("in list", properties);
+
+  if (loading) {
+    return <div>Loading...</div>; // 로딩 중일 때 출력할 내용
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>; // 에러 발생 시 출력할 내용
+  }
+
+  console.log("in list", properties);
   const renderedProperties = properties.map((property) => {
     return (
       <PropertyItem
